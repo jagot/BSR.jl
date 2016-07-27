e8d03653b97f3821ac09c71fd364406e3358c331
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-07-27 08:50:33 +0200

    Improved target loading

--- src/target.jl
+++ src/target.jl
@@ -77,14 +77,26 @@ function load_target(directory::AbstractString)
         end
         readline(file)
         expect_line()
-        read_parm(ASCIIString)
+        coupling = read_parm(ASCIIString)
         read_parm(Int)
         read_parm(Int)
         expect_line()
         ntarg = read_parm(Int)
         expect_line()
+        target[:targets] = []
         for i = 1:ntarg
-            readline(file)
+            line = split(readline(file))
+            t = Dict(:name => line[1],
+                     :parity => parse(Int, line[5]),
+                     :E => parse(Float64, line[6]),
+                     :ncfg => parse(Int, line[7]))
+            if coupling == "LS"
+                t[:L] = parse(Int, line[3])
+                t[:S] = parse(Int, line[4])
+            else
+                t[:J] = parse(Int, line[3])//2
+            end
+            push!(target[:targets], t)
         end
         expect_line()
         read_parm(Int)
@@ -93,11 +105,44 @@ function load_target(directory::AbstractString)
         expect_line()
         nlsp = read_parm(Int)
         expect_line()
-        for i = 1:nlsp
-            readline(file)
+        target[:partial_waves] = map(1:nlsp) do i
+            line = split(readline(file))
+            a,b = parse(Int, line[2]),parse(Int, line[3])
+            pw = Dict(:name => line[1],
+                      :parity => parse(Int, line[4]))
+            if coupling == "LS"
+                pw[:L] = a
+                pw[:S] = b
+            else
+                pw[:J] = a//2
+            end
+            pw
         end
         expect_line()
-        read_parm(Int)
+        kpert = read_parm(Int)
+        expect_line()
+        target[:perturbers] = Dict()
+        for i = 1:kpert
+            line = split(readline(file))
+            pk = parse(Int, line[1])
+            target[:perturbers][pk] = vcat(get(target[:perturbers], pk, [])..., line[2])
+        end
+        expect_line()
+        readline(file)
+        expect_line()
+        for i = 1:nlsp
+            line = split(readline(file))
+            nch = parse(Int, line[5])
+            target[:partial_waves][i][:channels] = map(1:nch) do j
+                line = split(readline(file))
+                Dict(:name => line[1],
+                     :l => parse(Int, line[2]),
+                     :target => parse(Int, line[3]),
+                     :last_cfg => parse(Int, line[5]),
+                     :K => parse(Int, line[6]))
+            end
+            expect_line()
+        end
         target
     end
 end
